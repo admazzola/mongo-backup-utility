81df380e631a0b651e5ce24254b95c82312caed8
andy <admazzola@gmail.com>
2022-03-18 13:55:51 -0400

    auto delete old directories

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ const path = require('path');
    
 
 
+let options = {
+    cronTime: process.env['cronTime'],
+    timeCutoff: process.env['timeCutoff']
+}
 
 function init(){
 
-    let options = {
-        cronTime: process.env['cronTime']
-
-    }
-
+     
 
     console.log('starting backup utility ', options)
 
@@ -51,17 +51,66 @@ function init(){
 
 function execute(){ 
 
+    let timeCutoff = options.timeCutoff ?  options.timeCutoff : 60*60*24*7   //every week
+ 
+    deleteObsoleteData(timeCutoff)
+
+    backupDatabases()
+
+ 
+
+}
+
+
+function backupDatabases(){
     let unixString = Date.now().toString()
 
     let folderName = 'backups/dump_'.concat(unixString)
 
       // Run external tool synchronously
-      if (shell.exec(`mongodump -o ${folderName}`).code !== 0) {
-        shell.echo('Error: shell command failed');
-        shell.exit(1);
+    if (shell.exec(`mongodump -o ${folderName}`).code !== 0) {
+     shell.echo('Error: shell command failed');
+     shell.exit(1);
     }
 
+}
 
+function getDirectories(path) {
+    return fs.readdirSync(path).filter(function (file) {
+      return fs.statSync(path+'/'+file).isDirectory();
+    });
+  }
+
+  
+
+function deleteObsoleteData(timeCutoff){
+
+    let directories = getDirectories( 'backups/' ).filter(x => x.startsWith('dump'))
+    
+    const now = Date.now() 
+
+    for(let name of directories){
+        let unixtime = parseInt( name.split('_')[1] )
+        console.log('unixtime',unixtime)
+
+        if(  now - unixtime > timeCutoff ){
+
+            deleteDirectory('backups/'+name)
+        }
+    }
+    console.log('directories ', directories)
+
+}
+
+function deleteDirectory(dirName){
+    fs.rmdir(dirName, { recursive: true }, (err) => {
+        if (err) {
+            throw err;
+        }
+    
+        console.log(`${dirName} was deleted!`);
+    });
+    
 }
 
 
